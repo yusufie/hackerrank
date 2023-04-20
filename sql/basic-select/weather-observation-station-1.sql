583f9acce49b2cc49704de89dae5769ff8a45c1e
yusuf <yusufpirejman@gmail.com>
2023-04-20 19:05:12 +0300

    SQL Basic Select Weather

---
+++ sql/basic-select/weather-observation-station-1.sql
@@ -0,0 +1,20 @@
+-- Hackerrank SQL Basic Select 
+-- Title : Weather Observation Station 1
+-- Query a list of CITY and STATE from the STATION table.
+-- The STATION table is described as follows:
+--
+--             STATION
+-- +------------------+---------------------+
+-- | Field            | Type                |
+-- +------------------+---------------------+
+-- | ID               | NUMBER              |
+-- | CITY             | VARCHAR2(21)        |
+-- | STATE            | VARCHAR2(2)         |
+-- | LAT_N            | NUMBER              |
+-- | LONG_W           | NUMBER              |
+-- +------------------+---------------------+
+--
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+--
+-- /* Enter your query here. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error. */
+SELECT CITY, STATE FROM STATION;
